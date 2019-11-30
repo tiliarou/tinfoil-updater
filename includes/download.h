6b93e2c0b3257cb00b1b6552c5fd3a831d77cb37
archbox <57974640+arch-box@users.noreply.github.com>
2019-11-29 22:46:50 -0700

    Update download.h
    added hosts for OFW files

--- includes/download.h
+++ includes/download.h
@@ -1,21 +1,8 @@
 #ifndef _DOWNLOAD_H_
 #define _DOWNLOAD_H_
 
-#define TINFOIL_URL         "https://tinfoil.io/repo/tinfoil.latest.zip"
-#define TINFOIL_NRO_URL     "https://tinfoil.media/repo/tinfoil.json"
-#define HBG_URL             "https://thehbg.shop/files/hbgshop.latest.zip"
-#define APP_URL             "https://github.com/JackInTheShop/tinfoil-updater/releases/latest/download/tinfoil-updater.nro"
-
-// legacy versions of tinfoil
-#define legacy500           "https://tinfoil.io/repo/tinfoil.500.nro"
-#define legacy410           "https://tinfoil.io/repo/tinfoil.410.nro"
-#define legacy400           "https://tinfoil.io/repo/tinfoil.400.nro"
-#define legacy302           "https://tinfoil.io/repo/tinfoil.302.nro"
-#define legacy301           "https://tinfoil.io/repo/tinfoil.301.nro"
-#define legacy300           "https://tinfoil.io/repo/tinfoil.300.nro"
-#define legacy210           "https://tinfoil.io/repo/tinfoil.210.nro"
-#define legacy201           "https://tinfoil.io/repo/tinfoil.201.nro"
-#define legacy200           "https://tinfoil.io/repo/tinfoil.200.nro"
+#define LATEST_URL         "https://www.underprivlidged.xyz/packs/OFWlatest.zip" // currently 9.0.1 as of November 29, 2019
+#define STABLE_URL         "https://www.underprivlidged.xyz/packs/OFWstable.zip" // currently 8.1.0 as of November 29, 2019
 
 int downloadFile(const char *url, const char *output);
 
